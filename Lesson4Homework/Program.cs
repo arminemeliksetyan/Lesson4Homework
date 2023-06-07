f4c6cb74ed0cf9a1e07fdbf022d4552f7ab2e761
Cute <Cute@DESKTOP-QCOA7R0>
2023-06-08 01:28:19 +0400

    Add project files.

---
+++ Lesson4Homework/Program.cs
@@ -0,0 +1,364 @@
+﻿using System;
+
+namespace Lesson4Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 21
+
+            //int a = 6;
+            //int b=77;
+            //int c=8;
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else if (b > c && b > a)
+            //{
+            //    Console.WriteLine(b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            // task 22
+
+            //int a = 26;
+            //int b = 77;
+            //int c = 8;
+            //if (a < b && a < c)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else if (b < c && b < a)
+            //{
+            //    Console.WriteLine(b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            //task 23    
+            //int a = 7; int b = 7; int c = 6;
+            //if (a == b || a == c || b == c)
+            //{
+            //    console.writeline("true");
+            //}
+            //else
+            //{
+            //    console.writeline("false");
+            //}
+
+
+            // task 24
+
+            //int two = 2;
+            //int a = 24; int b = 2; int c = 2;
+            //if (a == two && b == two || a == two && c== two || b == two && c == two)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+
+            //task 25
+
+            //int a = 15; int b = 2; int c = 3;
+
+            //if (a > 0 && b > 0 && c > 0)
+            //{
+            //    Console.WriteLine("y = 1");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("y = 2");
+            //}
+
+            // task 26
+
+            //int a = 1; int b = 2; int c = 3;
+            //if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0)
+            //{
+            //    Console.WriteLine("1");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("2");
+            //}
+
+            // task 27
+
+            //int a = 2; int b = 4; int c = 8;
+            //if (a + a == b && b + b == c)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //?? xndiry chhaskaca          // task 28
+
+            //chstacvec         // task 29
+
+            //int[] sum = new int[3];
+            //int a = 22; int b = 4; int c = 8;
+            //if (a > b && a > c)
+            //{
+            //    sum.SetValue(a, 0);
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    sum.SetValue(b, 1);
+            //}
+            //else if (c > a && c > b)
+            //{
+            //    sum.SetValue(c, 2);
+            //}
+            //for(int i = 0; i < sum.Length; i++)
+            //{
+            //    Console.WriteLine(sum[i]);
+            //}
+
+
+            // task 30 nuyn 29 i xndiet hakarak
+
+
+            // task 31 
+            //int a = 1; int b = 22; int c = 3; int d = 4;
+            //if (a > b && a > c && a > d)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else if (b > c && b > a && b > d)
+            //{
+            //    Console.WriteLine(b);
+            //}
+            //else if (c > a && c > b && c > d)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            //else 
+            //{
+            //    Console.WriteLine(d);
+            //}
+
+
+            // task 32
+
+            //int a = 1; int b = 22; int c = 3; int d = 4;
+            //if (a < b && a < c && a < d)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else if (b < c && b < a && b < d)
+            //{
+            //    Console.WriteLine(b);
+            //}
+            //else if (c < a && c < b && c < d)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(d);
+            //}
+
+            // task 33
+
+            //int a = 7; int b = 77; int c = 6; int d = 78;
+            //if (a == b || a == c || a == d || b == c || b == d || c == d)
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("false");
+            //}
+
+
+            //task 34
+
+            //int a = 7; int b = 7; int c = 67; int d = 6;
+            //if (a + b == c + d || a + d == b + c || a + c == b + d)
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("false");
+            //}
+
+
+            // task 35
+
+            //int a = 1; int b = 1; int c = 1; int d = 3;
+            //if (a + b + c == d || a + b + d == c || c + d + a == b || b + c + d == a)
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("false");
+            //}
+
+            //false          //task 36
+
+            //int a = 112; int b = 2; int c = 3; int d = 4;
+            //if(a % 2 == 1 && b % 2 == 1 || c % 2 == 1 || d % 2 == 1)
+            //{
+            //    Console.WriteLine(1);
+            //}else if(b % 2 == 1 && c % 2 == 1 || d % 2 == 1)
+            //{
+            //    Console.WriteLine(1);
+            //}else if(c % 2 == 1 && d % 2 == 1)
+            //{
+            //    Console.WriteLine(1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(2);
+            //}
+
+            // task 37
+
+            //int a = 2; int b = 4; int c = 8; int d = 116;
+            //if (a + a == b && b + b == c && c + c ==d)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //task 38 39 nuyn 28 29i xndirn e
+
+
+
+            //task 211 
+
+            //int[] ints = { 1, 2, 3, -5, -8 };
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < ints.Length; i++)
+            //{
+            //    if (ints[i] > 0)
+            //    {
+            //        count++;
+            //        sum += ints[i];
+            //    }
+            //}
+            //Console.WriteLine(sum / count);
+
+            // task 212
+
+            //int[] array = {5,5,56,-8,8,84};
+            //double sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] > 0)
+            //    {
+            //        sum += Math.Pow(array[i], 2); //hashvum e qarakusin 1in masum dnum enq tivy 2rdum qarakusu astichany
+            //        count++;
+            //    }
+            //}
+            //double average = Math.Sqrt(sum / count); // hanum e stacvac tvi armaty
+            //Console.WriteLine(average);
+
+            //task 213
+
+            //int[] array = { -5, 5, -5, -5, 8, 84 };
+            //double sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] < 0)
+            //    {
+            //        sum += Math.Pow(array[i], 2); //hashvum e qarakusin 1in masum dnum enq tivy 2rdum qarakusu astichany
+            //        count++;
+            //    }
+            //}
+            //double average = Math.Sqrt(sum / count); // hanum e stacvac tvi armaty
+            //Console.WriteLine(average);
+
+
+            // task 214 
+
+            //int[] ints = { 1, -2, -21, -22, -2 };
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < ints.Length; i++)
+            //{
+            //    if (ints[i] < 0)
+            //    {
+            //        count++;
+            //        sum += ints[i];
+            //    }
+            //}
+            //Console.WriteLine(sum / count);
+
+            //task 215
+
+            //int[] ints = { 1, 2, 3, 6, 8 };
+            //int sum = 0;
+            //for (int i = 2; i < ints.Length; i += 2)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+
+            //task 216
+
+            //int[] ints = { 1, 2, 3, 6, 8 };
+            //int sum = 1;
+            //for (int i = 2; i < ints.Length; i += 2)
+            //{
+            //    sum *= i;
+            //}
+            //Console.WriteLine(sum);
+
+            //task 217
+
+            //int[] ints = { 1, 2, 3, 6, 8 };
+            //int sum = 1;
+
+            //for (int i = 0; i < ints.Length; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        sum *= ints[i] * ints[i];
+            //    }
+            //}
+
+            //Console.WriteLine(sum);
+
+            // task 218
+
+            int[] ints = { 1, 2, 3, 6, 8 };
+            int sum = 0;
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (i % 2 != 0) 
+                {
+                    sum += ints[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
